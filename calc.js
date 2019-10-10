b18b6a501f9b14f33bada110975d45ded86d341f
ega <ega@terascode.com>
2019-10-10 15:42:53 +0700

    Add print feature

--- calc.js
+++ calc.js
@@ -2,6 +2,7 @@ const add = (a, b) => a + b
 const sub = (a, b) => a - b
 const mul = (a, b) => a * b
 const div = (a, b) => a / b
+const print = (text) => console.log(text)
 
 module.exports = {
   add,
